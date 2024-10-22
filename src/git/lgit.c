9c6c081a6d5215b532ebffff62ec55247f25c141
jmattaa <mattajonathan1@gmail.com>
2024-10-22 22:21:16 +0200

    trying to get staged files

--- src/git/lgit.c
+++ src/git/lgit.c
@@ -1,9 +1,13 @@
 #include "include/lgit.h"
+#include "include/utils.h"
+#include <stdio.h>
 #include <string.h>
+
+#define GIT_INDEX_HEAD_LEN 48 // in bits
 
 lgit_entries *lgit_getGitEntries(laser_opts opts)
 {
-    lgit_entries *entries = malloc(sizeof(lgit_entries *));
+    lgit_entries *entries = malloc(sizeof(lgit_entries));
 
     lgit_parseGit(opts.parentDir);
 
@@ -18,7 +22,33 @@ void lgit_parseGit(char *dir)
     snprintf(index_file, strlen(dir) + strlen("/.git/index") + 1,
              "%s/.git/index", dir);
 
-    printf("index file at: %s\n", index_file);
+    size_t idxfsize;
+    char *idx_bincontent = lgit_utils_readbin(index_file, &idxfsize);
+    char *idx_file_as_hex = lgit_utils_binToHex(idx_bincontent, idxfsize);
+
+    // https://git-scm.com/docs/index-format
+    // HEADER
+    // first 4 bytes DIRC
+    char HEADER_SIGNATURE[4] = {'D', 'I', 'R', 'C'};
+
+    if (strcmp(lgit_utils_getChars(idx_bincontent, 0, 4 * 8),
+               HEADER_SIGNATURE) != 0)
+    {
+        fprintf(stderr, "lsr: %s: file is not formatted right!!\n", index_file);
+        return;
+    }
+
+    // next 4 bytes: version
+    // 4 bytes = 8 hex nums
+    uint32_t version =
+        lgit_utils_hexToUint32(lgit_utils_getChars(idx_file_as_hex, 8, 8));
+
+    // then 32 bit num for index entries
+    // 32 bit 4 byte
+    uint32_t entries_num =
+        lgit_utils_hexToUint32(lgit_utils_getChars(idx_file_as_hex, 16, 8));
+
+    printf("version: %u, entries_count: %u\n", version, entries_num);
 }
 
 void lgit_entries_free(lgit_entries *lgit)
---
+++ src/git/utils.c
@@ -0,0 +1,128 @@
+#include "include/utils.h"
+#include <stdint.h>
+
+uint32_t lgit_utils_hexToUint32(const char *hex)
+{
+    return (uint32_t)strtoul(hex, NULL, 16);
+}
+
+uint32_t lgit_utils_binToUint32(const char *bin)
+{
+    return (int)strtol(bin, NULL, 2);
+}
+
+char *lgit_utils_readbin(char *filename, size_t *size)
+{
+    FILE *file = fopen(filename, "rb");
+    if (!file)
+    {
+        perror("Unable to open file");
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    if (fileSize <= 0)
+    {
+        fclose(file);
+        return NULL;
+    }
+
+    char *buffer = (char *)malloc(fileSize + 1);
+    if (!buffer)
+    {
+        fclose(file);
+        perror("Memory allocation failed");
+        return NULL;
+    }
+
+    size_t bytesRead = fread(buffer, 1, fileSize, file);
+    if (bytesRead != fileSize)
+    {
+        free(buffer);
+        fclose(file);
+        perror("Error reading file");
+        return NULL;
+    }
+
+    buffer[fileSize] = '\0';
+
+    fclose(file);
+    if (size)
+    {
+        *size = fileSize;
+    }
+
+    return buffer;
+}
+
+char *lgit_utils_binToHex(const char *bincontent, size_t size)
+{
+    // 1 byte = 2 hex
+    // then + null terminator
+    char *hexString = (char *)malloc(size * 2 + 1);
+    if (!hexString)
+    {
+        perror("Memory allocation failed");
+        return NULL;
+    }
+
+    for (size_t i = 0; i < size; ++i)
+        sprintf(hexString + i * 2, "%02X", (unsigned char)bincontent[i]);
+
+    hexString[size * 2] = '\0';
+    return hexString;
+}
+
+char *lgit_utils_getChars(const char *str, int n, int x)
+{
+    int length = strlen(str);
+
+    if (n < 0 || n >= length)
+    {
+        printf("Index n (%d) is out of bounds.\n", n);
+        return NULL;
+    }
+
+    if (n + x > length)
+    {
+        x = length - n;
+    }
+
+    char *result = (char *)malloc((x + 1) * sizeof(char));
+    if (result == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return NULL;
+    }
+
+    strncpy(result, str + n, x);
+    result[x] = '\0';
+
+    return result;
+}
+
+char *lgit_utils_getBinaryData(const char *buffer, size_t start, size_t length,
+                               size_t total_size)
+{
+    if (start < 0 || start >= total_size || start + length > total_size)
+    {
+        printf(
+            "Start (%zu) and length (%zu) out of bounds (total size: %zu).\n",
+            start, length, total_size);
+        return NULL;
+    }
+
+    char *result = (char *)malloc(length * sizeof(char));
+    if (result == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return NULL;
+    }
+
+    memcpy(result, buffer + start, length);
+
+    return result;
+}

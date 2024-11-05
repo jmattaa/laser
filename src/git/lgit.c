5e0e35c4b102223ff38305290e342d818b11c66d
EgorWeders <157705588+EgorWeders@users.noreply.github.com>
2024-11-05 12:49:56 -0500

    Fixes + github make action (#15)
    * Replace magic numbers for path size to default linux PATH_MAX
    
    * Add Branch_size define instead of magic number 8
    
    * Fixe mem leaks and parameter constness discard
    
    * Create makefile.yml
    
    * Fix not used steps
    
    * Add github action

--- src/git/lgit.c
+++ src/git/lgit.c
@@ -8,10 +8,7 @@
 
 char **lgit_parseGitignore(const char *dir, int *count)
 {
-    size_t filename_len =
-        strlen(dir) + strlen("/.gitignore") +
-        1;
-
+    size_t filename_len = strlen(dir) + strlen("/.gitignore") + 1;
     char *filename = malloc(filename_len);
 
     snprintf(filename, filename_len, "%s/.gitignore", dir);
@@ -69,51 +66,51 @@ char **lgit_parseGitignore(const char *dir, int *count)
 // lgit_entries *lgit_getGitEntries(laser_opts opts)
 // {
 //     lgit_entries *entries = malloc(sizeof(lgit_entries));
-// 
+//
 //     lgit_parseGit(opts.parentDir);
-// 
+//
 //     return entries;
 // }
-// 
+//
 // void lgit_parseGit(char *dir)
 // {
 //     // TODO: check if git dir exists!!
 //     char *index_file = malloc(strlen(dir) + strlen("/.git/index") + 1);
-// 
+//
 //     snprintf(index_file, strlen(dir) + strlen("/.git/index") + 1,
 //              "%s/.git/index", dir);
-// 
+//
 //     size_t idxfsize;
 //     char *idx_bincontent = lgit_utils_readbin(index_file, &idxfsize);
 //     char *idx_file_as_hex = lgit_utils_binToHex(idx_bincontent, idxfsize);
-// 
+//
 //     // https://git-scm.com/docs/index-format
 //     // HEADER
 //     // first 4 bytes DIRC
 //     char HEADER_SIGNATURE[4] = {'D', 'I', 'R', 'C'};
-// 
+//
 //     if (strcmp(lgit_utils_getChars(idx_bincontent, 0, 4 * 8),
 //                HEADER_SIGNATURE) != 0)
 //     {
 //         fprintf(stderr, "lsr: %s: file is not formatted right!!\n", index_file);
 //         return;
 //     }
-// 
+//
 //     // next 4 bytes: version
 //     // 4 bytes = 8 hex nums
 //     uint32_t version =
 //         lgit_utils_hexToUint32(lgit_utils_getChars(idx_file_as_hex, 8, 8));
-// 
+//
 //     // then 32 bit num for index entries
 //     // 32 bit 4 byte
 //     uint32_t entries_num =
 //         lgit_utils_hexToUint32(lgit_utils_getChars(idx_file_as_hex, 16, 8));
-// 
+//
 //     printf("version: %u, entries_count: %u\n", version, entries_num);
 // }
-// 
+//
 // void lgit_entries_free(lgit_entries *lgit)
 // {
 //     free(lgit);
 // }
-// 
+//

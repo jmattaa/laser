6fd0eb38e69283f933773a453d6c9e2078b52520
Jonathan Matta <mattajonathan1@gmail.com>
2024-11-21 21:38:58 +0100

    feat: add loading custom scripts from config

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 #include <lauxlib.h>
 #include <lua.h>
 #include <lualib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 lua_State *L;
 
@@ -26,15 +29,28 @@ lua_State *initialize_lua(const char *script_path)
 
 int main(int argc, char **argv)
 {
-    if (!initialize_lua("lua/laser.lua"))
-    {
-        fprintf(stderr, "lsr: lua error\n");
+    const char *default_script = "lua/laser.lua";
+    const char *user_script = getenv("HOME");
+    char user_config_path[LASER_PATH_MAX];
+
+    if (user_script)
+        snprintf(user_config_path, sizeof(user_config_path),
+                 "%s/.config/laser/laser.lua", user_script);
+    else
+        user_config_path[0] = '\0';
+
+    const char *script_to_load = default_script;
+
+    if (user_config_path[0] != '\0' && access(user_config_path, R_OK) == 0)
+        script_to_load = user_config_path;
+
+    if (!initialize_lua(script_to_load))
         return 1;
-    }
 
     laser_colors_init();
-
     laser_opts opts = laser_utils_parsecmd(argc, argv);
     laser_list_directory(opts, 0, opts.recursive_depth);
+
     return 0;
 }
+

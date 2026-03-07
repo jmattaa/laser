5c81d0fdf4ba998cc5f7cb211a9d8ce79e2caefc
Jonathan Matta <mattajonathan1@gmail.com>
2026-03-07 22:49:09 +0100

    scripts: update the `update-flake.sh` script

--- update-flake.sh
+++ update-flake.sh
@@ -17,9 +17,6 @@ sed -i.bak "s|rev = \".*\";|rev = \"${RELEASE_VERSION}\";|" flake.nix
 sed -i.bak "s|sha256 = \".*\";|sha256 = \"${NIX_SHA}\";|" flake.nix
 sed -i.bak "s|version = \".*\";|version = \"${RELEASE_VERSION#v}\";|" flake.nix
 
-git config user.name "github-actions[bot]"
-git config user.email "github-actions[bot]@users.noreply.github.com"
-
 git add flake.nix
 git commit -m "chore: update flake.nix for ${RELEASE_VERSION}"
-git push https://x-access-token:${GITHUB_TOKEN}@github.com/jmattaa/laser.git main
+git push 
